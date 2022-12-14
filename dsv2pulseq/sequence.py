writing pulseq sequences works now, but there is still a bug in the gradients and the interpolation has to be checked

dsv2pulseq/sequence.py
@@ -95,7 +95,7 @@ class Trig():
 
 class Sequence():
 
-    def __init__(self):
+    def __init__(self, ref_volt):
         self.n_blocks = 0
         self.duration = 0
         self.block_list = []
@@ -105,10 +105,20 @@ class Sequence():
         self.gy_val = np.array([])
         self.gz_val = np.array([])
 
-        self.rf_delta = 0 
-        self.gx_delta = 0 
-        self.gy_delta = 0 
-        self.gz_delta = 0 
+        self.delta = {'rf': 5, 'gx': 10, 'gy': 10, 'gz': 10}
+
+        # Conversion factors from dsv to (Py)Pulseq (SI) units
+        self.gamma = 42.576e6
+        self.cf_time = 1e-6 # [us] -> [s]
+        self.cf_grad = 1e-3*self.gamma # [mT/m] -> [Hz/m]
+
+        # ref_volt is voltage for 1ms 180 deg rectangular pulse
+        # pi[rad]/1[ms] = pi[rad]*1000[Hz] = 500[Hz] equals the reference voltage
+        # -> pulse [Hz] = pulse[V]/ref_volt[V] * 500[Hz]
+        self.cf_rf = 5e2 / ref_volt # [V] -> [Hz]
+
+        self.rf_raster = 1e-6
+        self.grad_raster = 1e-5
 
     def add_block(self, idx, duration, start_time):
         self.n_blocks += 1
@@ -125,10 +135,10 @@ class Sequence():
         self.gx_val = shapes[2].values
         self.gy_val = shapes[3].values
         self.gz_val = shapes[4].values
-        self.rf_delta = int(shapes[0].definitions.horidelta)
-        self.gx_delta = int(shapes[2].definitions.horidelta)
-        self.gy_delta = int(shapes[3].definitions.horidelta)
-        self.gz_delta = int(shapes[4].definitions.horidelta)
+        self.delta['rf'] = int(shapes[0].definitions.horidelta)
+        self.delta['gx']= int(shapes[2].definitions.horidelta)
+        self.delta['gy'] = int(shapes[3].definitions.horidelta)
+        self.delta['gz'] = int(shapes[4].definitions.horidelta)
 
     def get_shape(self, event):
         """
@@ -151,8 +161,12 @@ class Sequence():
         filename: Pulseq output filename
         """
 
+        # WIP: check interpolation of rf pulses & gradients
+        # WIP: check why gradients end up on wrong axes
+        # WIP: check sign of gradients (especially x)
+
         import pypulseq as pp
-        from pypulseq.make_arbitrary_grad import make_arbitrary_grad
+        from dsv2pulseq.helper import waveform_from_seqblock, round_up_to_raster
         import time
 
         print("Create Pulseq sequence file.")
@@ -160,105 +174,153 @@ class Sequence():
 
         filename = os.path.splitext(filename)[0] + '.seq'
 
-        # Conversion factors from dsv to (Py)Pulseq (SI) units
-        cf_time = 1e-6 # [us] -> [s]
-        cf_grad = 1e-3 # [mT/m] -> [T/m]
-        cf_rf = np.pi * 1e3 / ref_volt # [V] -> [Hz] - ref_volt is voltage for 1ms 180 deg rectangular pulse
-
-        rf_raster = 1e-6
-        grad_raster = 1e-5
-        delta = {'rf': self.rf_delta*cf_time, 'gx': self.gx_delta*cf_time, 'gy': self.gy_delta*cf_time, 'gz': self.gz_delta*cf_time}
-
         # set high system values to not produce any errors
-        system = pp.Opts(max_grad=1e4, max_slew=1e4, rf_raster_time=rf_raster, grad_raster_time=grad_raster, grad_unit='mT/m', slew_unit='mT/m/ms')
-
-        # track objects and duration
-        # time tracking is always done in us, as these are integers, time is only converted to [s], when creating a pulseq event
-        pp_block_dur = 0
-        event_check = {'rf': False, 'gx': False, 'gy': False, 'gz': False, 'adc': False, 'trig': False}
-        ts_offset = 0
+        system = pp.Opts(max_grad=1e4, max_slew=1e4, rf_raster_time=self.rf_raster, grad_raster_time=self.grad_raster, grad_unit='mT/m', slew_unit='mT/m/ms',
+                        rf_ringdown_time=0, rf_dead_time=0)
 
         pp_seq = pp.Sequence()
         pp_seq.set_definition('Name', filename)
         for block in self.block_list:
+            # track objects and duration
+            # time tracking is always done in us, as these are integers, time is only converted to [s], when creating a pulseq event
             pp_events = []
+            event_check = {'rf': 0, 'gx': 0, 'gy': 0, 'gz': 0, 'adc': 0, 'trig': 0}
+            ts_offset = 0
             for k,ts in enumerate(block.timestamps):
                 events = block.timestamps[ts]
+                concat_g = {'gx': False, 'gy': False, 'gz': False} # helper variable for gradient concatenation
 
                 # if the Pulseq block already contains an event of the same kind, the block has to be splitted
                 if any(event_check[event.type] for event in events):
-                    pp_events_tmp = []
-                    for pp_event in pp_events:
-                        pp_dur = int(np.round(pp.calc_duration(pp_event) * 1e6))
-                        if pp_dur > int(ts) - ts_offset:
-                            if hasattr(pp_event, 'waveform') or hasattr(pp_event, 'amplitude'):
-                                split_pt = (pp_dur - (int(ts) - ts_offset)) * cf_time
-                                g_new1, g_new2 = pp.split_gradient_at(pp_event, split_pt, system=system)
-                                pp_events_tmp.append(g_new2)
-                                pp_event = g_new1
-                            else:
-                                raise ValueError(f"ADC, RF or trigger event in block with index {block.block_idx} can not be splitted. This version only supports splitting of gradients")
-                    
-                    pp_seq.add_block(*pp_events)
-                    pp_events = pp_events_tmp
-                    ts_offset = int(ts)
+                    split = any((int(np.round(pp.calc_duration(pp_event) / self.cf_time)) > int(ts) - ts_offset) for pp_event in pp_events)
+                    if split:
+                        # if there is an event, that would have to be splitted, the two events of the same kind will be concatenated instead
+                        # it is however only possible to concatenate gradient objects
+                        check_adc = any(event.type=='adc' for event in events) and event_check['adc']
+                        check_rf = any(event.type=='rf' for event in events) and event_check['rf']
+                        if check_adc or check_rf:
+                            # split gradients
+                            pp_events_tmp = []
+                            for pp_event in pp_events:
+                                pp_dur = int(np.round(pp.calc_duration(pp_event) / self.cf_time))
+                                if pp_dur > int(ts) - ts_offset:
+                                    if hasattr(pp_event, 'waveform') or hasattr(pp_event, 'amplitude'):
+                                        split_pt = int(np.round((pp_dur - (int(ts) - ts_offset)) * self.cf_time/self.grad_raster))
+                                        g_wf = waveform_from_seqblock(pp_event)
+                                        g_new = pp.make_arbitrary_grad(channel=pp_event.channel, waveform=g_wf[:split_pt], delay=pp_event.delay, system=system)
+                                        pp_events_tmp.append(pp.make_arbitrary_grad(channel=pp_event.channel, waveform=g_wf[split_pt:], delay=0, system=system))
+                                        pp_event = g_new
+                                    else:
+                                        raise ValueError(f"ADC, RF or trigger event in block with index {block.block_idx} can not be splitted. This version only supports splitting of gradients")
+                            pp_seq.add_block(*pp_events)
+                            pp_events = pp_events_tmp
+                            ts_offset = int(ts)
+                            event_check.fromkeys(event_check,0)
+                        else:
+                            # concatenate gradients instead of splitting the event block
+                            for event in events:
+                                if event_check[event.type] is not None:
+                                    event_del = event.delay - ts_offset
+                                    pp_event = pp_events[event_check[event.type]-1] # find the existing gradient of the same channel
+                                    if event.type == 'trig':
+                                        raise ValueError(f"Trigger event in block with index {block.block_idx} can not be concatenated. This version only supports concatenation of gradients.")
+                                    elif event.type[0] == 'g':
+                                        g_conc = self.__make_pp_grad(event, event_del, system)
+                                        if g_conc is not None:
+                                            pp_events[event_check[event.type]-1] = pp.add_gradients([pp_event, g_conc], system=system)
+                                        concat_g[event.type] = True
+                                    else:
+                                        raise ValueError("Did not recognize event to be concatenated.")                                        
+                    else:
+                        # no splitting or concatenation of gradients necessary, just start a new block
+                        pp_seq.add_block(*pp_events)
+                        pp_events = []
+                        ts_offset = int(ts)
+                        event_check.fromkeys(event_check,0)
 
+                # add events to the block
                 for event in events:
-                    # add event to the block
                     event_del = event.delay - ts_offset
                     if event.type == 'rf':
-                        rf_sig = self.get_shape(event)
-                        rf_fac = int(np.round(delta[event.type]/rf_raster))
-                        rf_sig = np.interp(np.linspace(0,1,rf_fac*len(rf_sig)), np.linspace(0,1,len(rf_sig)), rf_sig)
-                        rf = pp.make_arbitrary_rf(signal=rf_sig*cf_rf, flip_angle=2*np.pi, delay=event_del*cf_time, freq_offset=event.freq, phase_offset=np.deg2rad(event.phase), system=system)
+                        rf = self.__make_pp_rf(event, event_del, system)
                         pp_events.append(rf)
                         event_dur = event_del + event.duration
-                    elif event.type == 'gx' or event.type == 'gy' or event.type == 'gz':
-                        if event.ramp_dn != 0:
-                            # trapezoid
-                            g_flat = (event.duration - event.ramp_dn) * cf_time
-                            g_area = event.amp * cf_grad * (g_flat + (event.ramp_up/2+event.ramp_dn/2) * cf_time)
-                            g = pp.make_trapezoid(channel=event.channel, amplitude=event.amp*cf_grad, area=g_area, flat_time=g_flat, rise_time=event.ramp_up*cf_time, delay=event_del*cf_time, system=system)
-                            g.fall_time = event.ramp_dn * cf_time # in make_trapezoid, its not possible to make an asymetric trapezoid, so we recalculate the area after setting the fall time explicitly
-                            g.area = g.amplitude * (g.flat_time + g.rise_time/2 + g.fall_time/2)
-                            event_dur = event_del + event.duration + event.ramp_dn
-                        elif event.duration == 0 and event.ramp_up == 0:
-                            # zero duration gradient
-                            pass
+                    elif (event.type == 'gx' or event.type == 'gy' or event.type == 'gz'):
+                        if concat_g[event.type]:
+                            concat_g[event.type] = False
                         else:
-                            # arbitrary
-                            g_wf = self.get_shape(event)
-                            g_fac = int(np.round(delta[event.type]/grad_raster))
-                            g_wf = np.interp(np.linspace(0,1,g_fac*len(g_wf)), np.linspace(0,1,len(g_wf)), g_wf)
-                            g = make_arbitrary_grad(channel=event.channel, waveform=g_wf*cf_grad, delay=event_del*cf_time, system=system)
-                            event_dur = event.duration + event_del
-                        pp_events.append(g)
+                            g = self.__make_pp_grad(event, event_del, system)
+                            if g is not None:
+                                pp_events.append(g)
+                                event_dur = event_del + event.duration 
+                                if hasattr(g, 'fall_time'):
+                                    event_dur += event.ramp_dn
                     elif event.type == 'adc':
-                        adc = pp.make_adc(num_samples=event.samples, duration=event.duration, delay=event_del*cf_time, freq_offset=event.freq, phase_offset=np.deg2rad(event.phase), system=system)
+                        adc_dur = round_up_to_raster(event.duration*self.cf_time, 6)
+                        adc_del = round_up_to_raster(event_del*self.cf_time, 6)
+                        adc = pp.make_adc(num_samples=event.samples, duration=adc_dur, delay=adc_del, freq_offset=event.freq, phase_offset=np.deg2rad(event.phase), system=system)
                         event_dur = event_del + event.duration
                         pp_events.append(adc)
                     elif event.type == 'trig':
                         if event.trig_type == 'EXTRIG0': # only external triggers supported atm
-                            trig = pp.make_digital_output_pulse(channel='ext1', duration=event.duration, delay=event_del*cf_time, system=system)
+                            trig_dur = round_up_to_raster(event.duration*self.cf_time, 6)
+                            trig_del = round_up_to_raster(event_del*self.cf_time, 6)
+                            trig = pp.make_digital_output_pulse(channel='ext1', duration=trig_dur, delay=trig_del, system=system)
                             pp_events.append(trig)
                             event_dur = event_del + event.duration
 
-                    event_check[event.type] = True
+                    # save position of event in list
+                    event_check[event.type] = len(pp_events)
 
-                    # calculate new block duration
-                    if event_dur > pp_block_dur:
-                        pp_block_dur = event_dur
-
-                    # add possible delay at end of Siemens block
-                    if k == len(block.timestamps)-1:
-                        ts_rel = int(ts) - ts_offset
-                        if ts_rel > pp_block_dur:
-                            block_delay = (ts_rel - pp_block_dur) * cf_time
-                            delay = pp.make_delay(d=block_delay, system=system)
-                            pp_events.append(delay)
-                    
+                # add possible delay at end of Siemens block
+                if k == len(block.timestamps)-1 and len(events) == 0:                       
+                    ts2 = list(block.timestamps.keys())
+                    block_delay = (int(ts2[-1]) - int(ts2[-2])) * self.cf_time
+                    delay = pp.make_delay(d=block_delay)
+                    pp_events.append(delay)
+                
             pp_seq.add_block(*pp_events)
 
         pp_seq.write(filename)
         end_time = time.time()
         print(f"Finished creating Pulseq file in {(end_time-start_time):.2f}s.")
+
+
+    def __make_pp_rf(self, rf_event, event_del, system):
+            """
+            Make a Pulseq RF event
+            """
+            import pypulseq as pp
+            from dsv2pulseq.helper import round_up_to_raster
+
+            rf_sig = self.get_shape(rf_event)
+            rf_fac = int(np.round(self.delta[rf_event.type]*self.cf_time/self.rf_raster))
+            rf_sig = np.interp(np.linspace(0,1,rf_fac*len(rf_sig)), np.linspace(0,1,len(rf_sig)), rf_sig)
+            rf_del = round_up_to_raster(event_del*self.cf_time, 6)
+            rf = pp.make_arbitrary_rf(signal=rf_sig, flip_angle=1, delay=rf_del, freq_offset=rf_event.freq, phase_offset=np.deg2rad(rf_event.phase), return_gz=False, system=system)
+            rf.signal = rf_sig * self.cf_rf # reset the signal as it gets scaled in make_arbitrary_rf
+            return rf
+
+    def __make_pp_grad(self, grad_event, event_del, system):
+        """
+        Make a Pulseq gradient event
+        """
+        import pypulseq as pp
+        from dsv2pulseq.helper import round_up_to_raster
+
+        if grad_event.ramp_dn != 0:
+            # trapezoid
+            g_flat = round_up_to_raster((grad_event.duration - grad_event.ramp_dn) * self.cf_time, 6)
+            g_ramp_up = round_up_to_raster(grad_event.ramp_up*self.cf_time, 6)
+            g_ramp_dn = round_up_to_raster(grad_event.ramp_dn*self.cf_time, 6)
+            g_del = round_up_to_raster(event_del*self.cf_time, 6)
+            return pp.make_trapezoid(channel=grad_event.channel, amplitude=grad_event.amp*self.cf_grad, flat_time=g_flat, rise_time=g_ramp_up, fall_time=g_ramp_dn, delay=g_del, system=system)
+        elif grad_event.duration == 0 and grad_event.ramp_up == 0:
+            # zero duration gradient
+            return None
+        else:
+            # arbitrary
+            g_wf = self.get_shape(grad_event)
+            g_fac = int(np.round(self.delta[grad_event.type]*self.cf_time/self.grad_raster))
+            g_wf = np.interp(np.linspace(0,1,g_fac*len(g_wf)), np.linspace(0,1,len(g_wf)), g_wf)
+            return pp.make_arbitrary_grad(channel=grad_event.channel, waveform=g_wf*self.cf_grad, delay=event_del*self.cf_time, system=system)