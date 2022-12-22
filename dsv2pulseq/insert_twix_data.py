51255f69f3e09647671436e9ff26f588a90086a6
mavel101 <marten.veldmann@hhu.de>
2022-12-22 12:42:43 +0100

    fix inserting data

--- dsv2pulseq/insert_twix_data.py
+++ dsv2pulseq/insert_twix_data.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
 import argparse
-from twixtools import read_twix, write_twix
+import twixtools
 
 defaults = {'out_file': 'merged_rawdata.dat'}
 
 def main(args):
 
-    file1 = read_twix(args.in_file_1, keep_syncdata_and_acqend=True)
-    file2 = read_twix(args.in_file_1, keep_syncdata_and_acqend=True)
+    file1 = twixtools.read_twix(args.in_file_1, keep_syncdata_and_acqend=True)
+    file2 = twixtools.read_twix(args.in_file_2, keep_syncdata_and_acqend=True)
 
     if len(file1[-1]['mdb']) != len(file2[-1]['mdb']):
         raise ValueError("Files have different number of measurement data blocks (mdb).")
 
-    for k,mdb in enumerate(file2[-1]['mdb']):
-        file1[-1]['mdb'][k].data[:] = mdb.data[:]
+    for k,mdb in enumerate(file1[-1]['mdb']):
+        if not mdb.is_flag_set('ACQEND') and not mdb.is_flag_set('SYNCDATA'):
+            mdb = mdb.convert_to_local()
+            mdb.data = file2[-1]['mdb'][k].data.copy()
+            file1[-1]['mdb'][k] = mdb
 
-    write_twix(file1, args.out_file, version_is_ve=True)
+    twixtools.write_twix(file1, args.out_file, version_is_ve=True)
 
 if __name__ == "__main__":
 
