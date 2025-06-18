fix orientation and scaling of trapezoidal gradients

dsv2pulseq/read_dsv.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import logging
+import os
 from dsv2pulseq.sequence import Sequence
 from dsv2pulseq.read_dsv_samples import DSVFile
 from dsv2pulseq.read_dsv_inf import read_dsv_inf
@@ -68,13 +69,16 @@ def check_dsv(file_prefix1, file_prefix2, time_shift=20):
     """ 
     Checks, if dsv files from converted sequence are the same as in the original sequence
 
-    Pulseq sequence shape values are usually shifted by 20us, as there is an additional global
-    freq/phase event in the beginning of the sequence.
-    
+    Pulseq sequence shape usually have two 20us long additional global
+    freq/phase event in the beginning and end of the sequence. This can be
+    compensated by the time_shift parameter.
+
+    If provided, also the ADC shapes are compared.
+
     Parameters:
         file_prefix1: dsv file prefix of the original sequence
         file_prefix2: dsv file prefix of the Pulseq sequence
-        time_shift: time shift in us, which is applied to the shapes of file_prefix2
+        time_shift: time shift in us for the Pulseq sequence
 
     Returns:
         seq1: Sequence object of the original sequence
@@ -85,90 +89,123 @@ def check_dsv(file_prefix1, file_prefix2, time_shift=20):
     seq1 = read_dsv(file_prefix1)
     seq2 = read_dsv(file_prefix2)
 
-    len_rf = min(len(seq1.rf), len(seq2.rf))
-    len_gx = min(len(seq1.gx), len(seq2.gx))
-    len_gy = min(len(seq1.gy), len(seq2.gy))
-    len_gz = min(len(seq1.gz), len(seq2.gz))
+    if os.path.exists(file_prefix1 + "_ADC.dsv") and os.path.exists(file_prefix2 + "_ADC.dsv"):
+        adc1_dsv = DSVFile(file_prefix1 + "_ADC.dsv")
+        adc2_dsv = DSVFile(file_prefix2 + "_ADC.dsv")
+        adc_delta = adc1_dsv.definitions.horidelta
+        seq1.adc = adc1_dsv.values
+        seq2.adc = adc2_dsv.values
+    else:
+        seq1.adc = None
+        seq2.adc = None
 
     rfd1 = abs(seq1.rf)
     rfp1 = np.angle(seq1.rf)
     grx1 = seq1.gx
     gry1 = seq1.gy
     grz1 = seq1.gz
+    adc1 = seq1.adc
 
     rfd2 = abs(seq2.rf)
     rfp2 = np.angle(seq2.rf)
     grx2 = seq2.gx
     gry2 = seq2.gy
     grz2 = seq2.gz
+    adc2 = seq2.adc
 
-    shift_rf = -1* int(time_shift / seq1.delta_rf)
-    shift_grad = -1* int(time_shift / seq1.delta_grad)
+    shift_rf = int(time_shift / seq1.delta_rf)
+    shift_grad = int(time_shift / seq1.delta_grad)
 
-    rfd2 = np.roll(rfd2, shift_rf)
-    rfp2 = np.roll(rfp2, shift_rf)
-    grx2 = np.roll(grx2, shift_grad)
-    gry2 = np.roll(gry2, shift_grad)
-    grz2 = np.roll(grz2, shift_grad)
+    rfd2 = rfd2[shift_rf:-shift_rf]
+    rfp2 = rfp2[shift_rf:-shift_rf]
+    grx2 = grx2[shift_grad:-shift_grad]
+    gry2 = gry2[shift_grad:-shift_grad]
+    grz2 = grz2[shift_grad:-shift_grad]
+    if adc1 is not None and adc2 is not None:
+        shift_adc = -1* int(time_shift / adc_delta)
+        adc2 = adc2[shift_adc:]
+        subplots = 6
+    else:
+        subplots = 5
+
+    len_rf = min(len(rfd1), len(rfd2))
+    len_gx = min(len(grx1), len(grx2))
+    len_gy = min(len(gry1), len(gry2))
+    len_gz = min(len(grz1), len(grz2))
+    if adc1 is not None and adc2 is not None:
+        len_adc = min(len(adc1), len(adc2))
 
     # Plot sequence 1
-    plt.figure(figsize=(10, 8))
+    n_subplots = subplots  # 5 or 6
+    plt.figure(figsize=(10, 2 * n_subplots))
     plt.suptitle(f"Sequence 1: {file_prefix1}", fontsize=16)
-    plt.subplot(511)
+    plt.subplot(n_subplots, 1, 1)
     plt.plot(rfd1)
     plt.title("RFD")
-    plt.subplot(512)
+    plt.subplot(n_subplots, 1, 2)
     plt.plot(rfp1)
     plt.title("RFP")
-    plt.subplot(513)
+    plt.subplot(n_subplots, 1, 3)
     plt.plot(grx1)
     plt.title("GX")
-    plt.subplot(514)
+    plt.subplot(n_subplots, 1, 4)
     plt.plot(gry1)
     plt.title("GY")
-    plt.subplot(515)
+    plt.subplot(n_subplots, 1, 5)
     plt.plot(grz1)
     plt.title("GZ")
+    if n_subplots == 6:
+        plt.subplot(n_subplots, 1, 6)
+        plt.plot(adc1)
+        plt.title("ADC")
     plt.tight_layout(rect=[0, 0, 1, 0.96])
 
     # Plot sequence 2
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(10, 2 * n_subplots))
     plt.suptitle(f"Sequence 2: {file_prefix2}", fontsize=16)
-    plt.subplot(511)
+    plt.subplot(n_subplots, 1, 1)
     plt.plot(rfd2)
     plt.title("RFD")
-    plt.subplot(512)
+    plt.subplot(n_subplots, 1, 2)
     plt.plot(rfp2)
     plt.title("RFP")
-    plt.subplot(513)
+    plt.subplot(n_subplots, 1, 3)
     plt.plot(grx2)
     plt.title("GX")
-    plt.subplot(514)
+    plt.subplot(n_subplots, 1, 4)
     plt.plot(gry2)
     plt.title("GY")
-    plt.subplot(515)
+    plt.subplot(n_subplots, 1, 5)
     plt.plot(grz2)
     plt.title("GZ")
+    if n_subplots == 6:
+        plt.subplot(n_subplots, 1, 6)
+        plt.plot(adc2)
+        plt.title("ADC")
     plt.tight_layout(rect=[0, 0, 1, 0.96])
 
     # Plot difference
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(10, 2 * n_subplots))
     plt.suptitle(f"Difference: {file_prefix1} - {file_prefix2}", fontsize=16)
-    plt.subplot(511)
+    plt.subplot(n_subplots, 1, 1)
     plt.plot(rfd1[:len_rf] - rfd2[:len_rf])
     plt.title("RFD")
-    plt.subplot(512)
+    plt.subplot(n_subplots, 1, 2)
     plt.plot(rfp1[:len_rf] - rfp2[:len_rf])
     plt.title("RFP")
-    plt.subplot(513)
+    plt.subplot(n_subplots, 1, 3)
     plt.plot(grx1[:len_gx] - grx2[:len_gx])
     plt.title("GX")
-    plt.subplot(514)
+    plt.subplot(n_subplots, 1, 4)
     plt.plot(gry1[:len_gy] - gry2[:len_gy])
     plt.title("GY")
-    plt.subplot(515)
+    plt.subplot(n_subplots, 1, 5)
     plt.plot(grz1[:len_gz] - grz2[:len_gz])
     plt.title("GZ")
+    if n_subplots == 6:
+        plt.subplot(n_subplots, 1, 6)
+        plt.plot(adc1[:len_adc] - adc2[:len_adc])
+        plt.title("ADC")
     plt.tight_layout(rect=[0, 0, 1, 0.96])
 
     plt.show()

dsv2pulseq/sequence.py
@@ -8,12 +8,6 @@ import pypulseq as pp
 from dsv2pulseq.helper import round_up_to_raster, waveform_from_seqblock
 
 
-def init_event_dict():
-    return {'rf': None, 'gx': None, 'gy': None, 'gz': None, 'adc': None, 'trig': None, 'delay': None}
-
-def extract_events(event_dict):
-    return [event for event in event_dict.values() if event is not None]
-
 class Block():
     """
     Event block in "Siemens style", allows more than one object per channel
@@ -79,7 +73,7 @@ class Grad():
     def __init__(self, channel, amp, duration, ramp_up, ramp_dn, shape_ix):
         self.type = 'g' + channel 
         self.channel = channel
-        self.amp = amp
+        self.amp = amp # amplitude in logical! coordinate system
         self.duration = duration # flat_top + ramp_up
         self.ramp_up = ramp_up
         self.ramp_dn = ramp_dn
@@ -124,7 +118,7 @@ class Sequence():
         # Conversion factors from dsv to (Py)Pulseq (SI) units
         self.gamma = 42.576e6
         self.cf_time = 1e-6 # [us] -> [s]
-        self.cf_grad = -1* 1e-3*self.gamma # [mT/m] -> [Hz/m], the "-1" is to be compatible to the "XYZ in TRA" mode of Pulseq
+        self.cf_grad = 1e-3*self.gamma # [mT/m] -> [Hz/m]
 
         # ref_volt is voltage for 1ms 180 deg rectangular pulse
         # -> pulse [Hz] = pulse[V]/ref_volt[V] * 500[Hz]
@@ -217,7 +211,7 @@ class Sequence():
         pp_seq = pp.Sequence(system=system)
         pp_seq.set_definition('Name', os.path.basename(filename))
 
-        pulseq_events = init_event_dict()
+        pulseq_events = self.__init_event_dict()
         ts_offset = 0
 
         if not self.block_list:
@@ -245,7 +239,7 @@ class Sequence():
                         check_trig = any(event.type == 'trig' for event in events) and pulseq_events['trig'] is not None
                         split_pt = round(pulseq_time / self.delta_grad)
                         if check_adc or check_rf or check_trig: # split block
-                            pulseq_events_tmp = init_event_dict()
+                            pulseq_events_tmp = self.__init_event_dict()
                             for key, pp_event in pulseq_events.items():
                                 if pp_event is None:
                                     continue
@@ -257,7 +251,7 @@ class Sequence():
                                         pulseq_events[key] = g_pre
                                     else:
                                         raise ValueError(f"Event with type {pp_event.type} in block with index {block.block_idx} cannot be split. Only gradients can be split.")
-                            pp_seq.add_block(*extract_events(pulseq_events))
+                            pp_seq.add_block(*self.__extract_events(pulseq_events))
                             pulseq_events = pulseq_events_tmp.copy()
                             ts_offset = round(int(ts) / self.delta_grad) * self.delta_grad
                         else: # concatenate gradients
@@ -276,21 +270,21 @@ class Sequence():
                                             len_wf = max([len(wf) for wf in g_wf])
                                             g_wf = [np.concatenate([wf, np.zeros(len_wf - len(wf))]) for wf in g_wf]
                                             g_wf = g_wf[0] + g_wf[1]
-                                            pulseq_events[event.type] = pp.make_arbitrary_grad(channel=event.channel, waveform=g_wf, system=system)
+                                            pulseq_events[event.type] = self.__make_arbitrary_grad(g_wf, event.channel, 0, system=system)
                                         concat_g[event.type] = True
                                     else:
                                         raise ValueError("Unrecognized event type during concatenation.")
                     else: # no split needed, start new block
-                        block_dur = pp.calc_duration(*extract_events(pulseq_events))
-                        block_dur_rounded = round_up_to_raster(pp.calc_duration(*extract_events(pulseq_events)), 5)
+                        block_dur = pp.calc_duration(*self.__extract_events(pulseq_events))
+                        block_dur_rounded = round_up_to_raster(pp.calc_duration(*self.__extract_events(pulseq_events)), 5)
                         if abs(block_dur - block_dur_rounded) > 1e-7:
                             pulseq_events['delay'] = pp.make_delay(block_dur_rounded)
-                        pp_seq.add_block(*extract_events(pulseq_events))
+                        pp_seq.add_block(*self.__extract_events(pulseq_events))
 
                         # Add remaining delay to the new block
                         delay = round_up_to_raster((int(ts) - ts_offset) * self.cf_time, 5)
                         delay_remain = delay - block_dur_rounded
-                        pulseq_events = init_event_dict()
+                        pulseq_events = self.__init_event_dict()
                         pulseq_events['delay'] = pp.make_delay(delay_remain)
                         ts_offset += round(block_dur_rounded  / self.cf_time)
 
@@ -331,7 +325,7 @@ class Sequence():
         if self.__check_delay(pulseq_events, delay):
             pulseq_events['delay'] = pp.make_delay(delay)  # account for possible delay in last block
         if any(pulseq_events.values()):
-            pp_seq.add_block(*extract_events(pulseq_events))
+            pp_seq.add_block(*self.__extract_events(pulseq_events))
 
         # check timing of the sequence
         # set raster time to 1us for timing check, as otherwise ADC delays will throw errors
@@ -347,6 +341,12 @@ class Sequence():
         logging.info(f"Finished creating Pulseq file in {(end_time - start_time):.2f}s.")
 
         return pp_seq
+
+    def __init_event_dict(self):
+        return {'rf': None, 'gx': None, 'gy': None, 'gz': None, 'adc': None, 'trig': None, 'delay': None}
+
+    def __extract_events(self, event_dict):
+        return [event for event in event_dict.values() if event is not None]
 
     def __make_pp_rf(self, rf_event, event_del, system):
             """
@@ -364,31 +364,29 @@ class Sequence():
         Make a Pulseq gradient event
         """
 
+        g_wf = self.get_shape(grad_event) * self.cf_grad
+        g_del = round_up_to_raster(event_del*self.cf_time, 5)
         if grad_event.ramp_dn != 0:
             # trapezoid
+            g_amp = g_wf[np.argmax(np.abs(g_wf))]
             g_flat = round_up_to_raster((grad_event.duration - grad_event.ramp_up) * self.cf_time, 5)
+            if g_flat == 0:
+                g_amp += (g_wf[1] - g_wf[0]) / 2 # correct amp for special case of a triangular gradient
             g_ramp_up = round_up_to_raster(grad_event.ramp_up*self.cf_time, 5)
             g_ramp_dn = round_up_to_raster(grad_event.ramp_dn*self.cf_time, 5)
-            g_del = round_up_to_raster(event_del*self.cf_time, 5)
-            return pp.make_trapezoid(channel=grad_event.channel, amplitude=grad_event.amp*self.cf_grad, flat_time=g_flat, rise_time=g_ramp_up, fall_time=g_ramp_dn, delay=g_del, system=system)
+            return pp.make_trapezoid(channel=grad_event.channel, amplitude=g_amp, flat_time=g_flat, rise_time=g_ramp_up, fall_time=g_ramp_dn, delay=g_del, system=system)
         elif grad_event.duration == 0 and grad_event.ramp_up == 0:
             # zero duration gradient
             return None
         else:
-            # arbitrary
-            g_wf = self.get_shape(grad_event)
-            return pp.make_arbitrary_grad(channel=grad_event.channel, waveform=g_wf*self.cf_grad, delay=event_del*self.cf_time, system=system)
+            return self.__make_arbitrary_grad(g_wf, grad_event.channel, delay=g_del, system=system)
 
-    def __split_gradients(self, grad_event, split_pt, system):
-        """
-        Split gradients at the split point.
-        """
-        def create_grad(wf, channel, system):
-            grad = pp.make_arbitrary_grad(channel=channel, waveform=wf, delay=0, system=system)
-            return grad
-
-        # Pypulseq throws an error if the waveform has only one point.
-        def fix_single_point_grad(wf, channel, system):
+    def __make_arbitrary_grad(self, wf, channel, delay, system):
+        
+        if len(wf) > 1:
+            grad = pp.make_arbitrary_grad(channel=channel, waveform=wf, delay=delay, system=system)
+        else:
+            # Pypulseq throws an error if the waveform has only one point.
             grad = pp.make_arbitrary_grad(
                 channel=channel,
                 waveform=np.concatenate([wf, np.zeros(1, dtype=float)]),
@@ -401,35 +399,32 @@ class Sequence():
             grad.first = wf[0]
             grad.last = wf[0]
             grad.area = np.sum(wf * system.grad_raster_time)
-            return grad
+        return grad
+
+    def __split_gradients(self, grad_event, split_pt, system):
+        """
+        Split gradients at the split point.
+        """
 
         g_wf = waveform_from_seqblock(grad_event, system=system) # already contains delay
         g_pre_wf = g_wf[:split_pt]
         g_post_wf = g_wf[split_pt:]
 
-        if len(g_pre_wf) == 1:
-            g_pre = fix_single_point_grad(g_pre_wf, grad_event.channel, system)
-        else:
-            g_pre = create_grad(g_pre_wf, grad_event.channel, system)
-
-        if len(g_post_wf) == 1:
-            g_post = fix_single_point_grad(g_post_wf, grad_event.channel, system)
-        else:
-            g_post = create_grad(g_post_wf, grad_event.channel, system)
-
+        g_pre = self.__make_arbitrary_grad(g_pre_wf, grad_event.channel, 0, system)
+        g_post = self.__make_arbitrary_grad(g_post_wf, grad_event.channel, 0, system)
         return g_pre, g_post
 
     def __check_delay(self, pulseq_events, delay):
         """
         Check if delay is the longest delay in the current event block
         """
-        pulseq_events = extract_events(pulseq_events)
+        pulseq_events = self.__extract_events(pulseq_events)
         delays = [item.delay for item in pulseq_events if (item.type == "delay" and item is not None)] + [0]
         if delay > max(delays):
             return True
         else:
             return False
-        
+    
     def shift_timestamps(self):
         """
         Shift RF events by the RF lead time and ADC events by the ADC dead time