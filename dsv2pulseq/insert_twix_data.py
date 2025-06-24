c937bf748b556c87b4ef9ed902165caafdf61881
Marten Veldmann <marten.veldmann@dzne.de>
2025-06-24 17:41:14 +0200

    correct help string

--- dsv2pulseq/insert_twix_data.py
+++ dsv2pulseq/insert_twix_data.py
@@ -7,8 +7,8 @@ defaults = {'out_file': 'merged_rawdata.dat'}
 
 def main(args):
 
-    file1 = twixtools.read_twix(args.in_file_1, keep_syncdata_and_acqend=True)
-    file2 = twixtools.read_twix(args.in_file_2, keep_syncdata_and_acqend=True)
+    file1 = twixtools.read_twix(args.in_file_1, keep_syncdata=True, keep_acqend=True)
+    file2 = twixtools.read_twix(args.in_file_2, keep_syncdata=True, keep_acqend=True)
 
     if len(file1[-1]['mdb']) != len(file2[-1]['mdb']):
         raise ValueError("Files have different number of measurement data blocks (mdb).")
@@ -25,8 +25,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Create Pulseq sequence file from dsv file.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('in_file_1', type=str, help="Input Siemens raw data file acquired with original sequence.")
-    parser.add_argument('in_file_2', type=str, help="Input Siemens raw data file acquired with original sequence.")
+    parser.add_argument('in_file_1', type=str, help="Siemens raw data file acquired with original sequence.")
+    parser.add_argument('in_file_2', type=str, help="Pulseq raw data file.")
     parser.add_argument('-o', '--out_file', type=str, help='Output Siemens raw data file name. Default: merged_rawdata.dat')
 
     parser.set_defaults(**defaults)
