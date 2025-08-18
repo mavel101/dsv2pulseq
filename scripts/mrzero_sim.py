873f8a053ec5cbf241a8cb9a354c5a7ece70dea9
Marten Veldmann <marten.veldmann@dzne.de>
2025-08-18 17:02:29 +0200

    scale factor for simulation data, sensititvity maps with BART

--- scripts/mrzero_sim.py
+++ scripts/mrzero_sim.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 import MRzeroCore as mr0
 import twixtools
-import h5py
 import torch
 
 seq_path = '../test/test_data/'
@@ -17,6 +16,7 @@ plot_phantom = True
 plot_slc = 0
 
 insert_raw = False # insert simulation data into raw data file
+scale_fac = 1e-4 # data scale factor for insertion
 raw_path = ''
 raw_file = ''
 out_path = ''
@@ -31,13 +31,21 @@ obj_p.B0[:] = 0
 # Load sensitivity maps
 # 32 Biot-Savart coils from JEMRIS with extent 256, array radius 256, 3D
 if include_sens:
-    sensmaps = h5py.File('sensmaps.h5', 'r')
-    maps = sensmaps['maps']
-    coil_keys = sorted(maps['magnitude'].keys()) 
-    maps_mag = np.stack([maps['magnitude'][k][()] for k in coil_keys])
-    maps_phase = np.stack([maps['phase'][k][()] for k in coil_keys])
-    maps_cplx = maps_mag * np.exp(1j * maps_phase)
-    obj_p.coil_sens = torch.tensor(maps_cplx, dtype=torch.complex64)
+    from bart import bart
+
+    refdata = np.load('refdata_coils.npy')
+    sens_caldir = bart(1, 'caldir 32', refdata).T
+    sens = bart(1, f'resize -c 1 {18} 2 {18} 3 {48}', sens_caldir)
+
+    # import h5py
+    # sensmaps = h5py.File('sensmaps.h5', 'r')
+    # maps = sensmaps['maps']
+    # coil_keys = sorted(maps['magnitude'].keys()) 
+    # maps_mag = np.stack([maps['magnitude'][k][()] for k in coil_keys])
+    # maps_phase = np.stack([maps['phase'][k][()] for k in coil_keys])
+    # sens = maps_mag * np.exp(1j * maps_phase)
+
+    obj_p.coil_sens = torch.tensor(sens, dtype=torch.complex64)
 
 # interpolate and plot
 obj_p = obj_p.interpolate(*brain_phantom_res)
@@ -58,15 +66,16 @@ signal = mr0.execute_graph(graph, seq0, phantom, print_progress=True)
 
 #%%
 signal_numpy = signal.cpu().numpy()
+signal_numpy_scaled = signal_numpy * scale_fac
 
 plt.figure()
 plt.title('Signal magnitude')
-plt.plot(np.abs(signal_numpy))
+plt.plot(np.abs(signal_numpy_scaled))
 
 plt.figure()
 plt.title('Signal real and imaginary parts')
-plt.plot(signal_numpy.real, label='real')
-plt.plot(signal_numpy.imag, label='imag')
+plt.plot(signal_numpy_scaled.real, label='real')
+plt.plot(signal_numpy_scaled.imag, label='imag')
 
 # %%
 
@@ -76,20 +85,20 @@ if insert_raw:
     raw_data = twixtools.read_twix(raw, keep_syncdata=True, keep_acqend=True)
 
     nc = raw_data[-1]['mdb'][0].data.shape[0]
-    if nc != signal_numpy.shape[1]:
-        print(f"Number of coils {nc} in raw data file does not match simulated coils {signal_numpy.shape[0]}.")
+    if nc != signal_numpy_scaled.shape[1]:
+        print(f"Number of coils {nc} in raw data file does not match simulated coils {signal_numpy_scaled.shape[0]}.")
 
     sample_ix = 0
     for k, mdb in enumerate(raw_data[-1]['mdb']):
         if not mdb.is_flag_set('ACQEND') and not mdb.is_flag_set('SYNCDATA') and not mdb.is_flag_set('NOISEADJSCAN'):
             mdb = mdb.convert_to_local()
             mdb_samples = mdb.data.shape[1]
-            mdb.data = signal_numpy[sample_ix:sample_ix + mdb_samples,:].T
+            mdb.data = signal_numpy_scaled[sample_ix:sample_ix + mdb_samples,:].T
             raw_data[-1]['mdb'][k] = mdb
             sample_ix += mdb_samples
 
-    if sample_ix != signal_numpy.shape[0]:
-        print(f"MDB data length {sample_ix} does not match simulated signal length {signal_numpy.shape[0]}.")
+    if sample_ix != signal_numpy_scaled.shape[0]:
+        print(f"MDB data length {sample_ix} does not match simulated signal length {signal_numpy_scaled.shape[0]}.")
 
     print(f"Writing modified raw data to {out_file}.")
     out = os.path.join(out_path, out_file)
